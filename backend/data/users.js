5ded9aad602cfa51897d740a699a7644e7f787da
mohamedghaly140 <mohamedghaly140@gmail.com>
2021-03-05 19:23:07 +0200

    add error handler middleware"

--- backend/data/users.js
+++ backend/data/users.js
@@ -3,17 +3,20 @@ import bcrypt from 'bcryptjs';
 const users = [
 	{
 		name: 'Admin User',
+		username: 'admin1',
 		email: 'admin@example.com',
 		password: bcrypt.hashSync('123456', 12),
 		isAdmin: true,
 	},
 	{
 		name: 'John Doe',
+		username: 'johndoe99',
 		email: 'john@example.com',
 		password: bcrypt.hashSync('123456', 12),
 	},
 	{
 		name: 'Jane Doe',
+		username: 'jane2021',
 		email: 'jane@example.com',
 		password: bcrypt.hashSync('123456', 12),
 	},
